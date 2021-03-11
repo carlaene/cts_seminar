52130046d84d71419c0f81b1963625571c334872
maria <maria@DESKTOP-OGTF2OO>
2021-03-11 12:37:32 +0200

    part1

--- cts3/src/ase/ro/cts/laboratoare/Produs.java
+++ cts3/src/ase/ro/cts/laboratoare/Produs.java
@@ -1,5 +1,28 @@
 package ase.ro.cts.laboratoare;
 
 public class Produs {
-
-}
+	public static final int VECHIME_CLIENT_MAXIMA=10;
+	public static final float DISCOUNT_CLIENT_MAXIM=0.15f;
+		public float calculeazaPretFinal(int tipProdus, float pretinitial, int vechimeClientInAni)
+		  {
+		    float pretFinal = 0;
+		    float discountFidelitate = (vechimeClientInAni > VECHIME_CLIENT_MAXIMA) ? DISCOUNT_CLIENT_MAXIM : (float)vechimeClientInAni/100; 
+		    if (tipProdus== TipProdus.NOU)
+		    {
+		      pretFinal = pretinitial;
+		    }
+		    else if (tipProdus == TipProdus.DISCOUNT)
+		    {
+		      pretFinal = (pretinitial - (0.1f * pretinitial)) - discountFidelitate * (pretinitial - (0.1f * pretinitial));
+		    }
+		    else if (tipProdus == TipProdus.STOC_LIMITAT)
+		    {
+		      pretFinal = (pretinitial - (0.25f * pretinitial)) - discountFidelitate * (pretinitial - (0.25f * pretinitial));
+		    }
+		    else if (tipProdus == TipProdus.VECHI)
+		    {
+		      pretFinal = (pretinitial - (0.35f * pretinitial)) - discountFidelitate * (pretinitial - (0.35f * pretinitial));
+		    }
+		    return pretFinal;
+		  }
+	}
--- cts3/src/ase/ro/cts/laboratoare/TipProdus.java
+++ cts3/src/ase/ro/cts/laboratoare/TipProdus.java
@@ -1,5 +1,12 @@
 package ase.ro.cts.laboratoare;
 
 public enum TipProdus {
-
+	NOU(0),DISCOUNT(0.1f),STOC_LIMITAT(0.25f),VECHI(0.35f); //referinte catre obiecte
+private final float discount;
+private TipProdus(float discount) {
+	this.discount=discount;
+}
+public float getDiscount() {
+	return this.discount;
+}
 }
