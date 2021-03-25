8a8d1e65f87ac13853a1b4040904a22cfbe530ef
maria <maria@DESKTOP-OGTF2OO>
2021-03-25 12:46:09 +0200

    lab5-part2

--- Laborator5/src/ro/ase/csie/cts/dp/singleton/ConexiuneDB.java
+++ Laborator5/src/ro/ase/csie/cts/dp/singleton/ConexiuneDB.java
@@ -9,9 +9,9 @@ public ConexiuneDB(String ip, String denumire) {
 	this.ip = ip;
 	this.denumire = denumire;
 }
-public static ConexiuneDB getConexiune() {//static ca sa o putem apela
+public static synchronized ConexiuneDB getConexiune() {//static ca sa o putem apela
 if(conexiune==null) {
-	//datele se pot prelua din fis de configurare
+//datele se pot prelua din fis de configurare
 	conexiune= new ConexiuneDB("10.0.0.1", "cts");
 	
 }
